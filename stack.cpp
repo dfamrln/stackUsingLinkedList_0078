e2180c987bce84b0f7990055382aa5860395aaa7
dfamrln <mhdffa1@gmail.com>
2025-06-05 15:42:50 +0700

    class stack

--- stack.cpp
+++ stack.cpp
@@ -15,3 +15,15 @@ public:
 };
 
 // stack class
+class Stack
+{
+private:
+    Node *top; // pointer to the top node of the stack
+
+public:
+    Stack()
+    {
+        top = NULL; // initialize the stack with a null top pointer
+    }
+
+    // push operation insert an element onto the top of the stack
