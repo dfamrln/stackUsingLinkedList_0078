d84d7ba166b05303082cb86cd542624976f444a4
dfamrln <mhdffa1@gmail.com>
2025-06-05 17:35:45 +0700

    push

--- stack.cpp
+++ stack.cpp
@@ -25,4 +25,7 @@ public:
     {
         top = NULL; // initialize the stack with a null top pointer
     }
+
+     // push operation insert an element onto the top of the stack
+    int push(int value)
 }
