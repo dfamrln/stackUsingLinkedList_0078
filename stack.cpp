d1fe80b8e5b4a0ee7ca1ba1e9ec123d857e1d18a
dfamrln <mhdffa1@gmail.com>
2025-06-05 17:41:33 +0700

    returntop

--- stack.cpp
+++ stack.cpp
@@ -38,4 +38,7 @@ public:
     }
      // IsEmpty operation: check if the stack is empty
     bool isEmpty()
+    {
+        return top == NULL; // return true if the top pointer is NULL, indicating an empty stack
+    }
 }
