9d9e29594c9898ed6f6038af1e0a305ec141e0f9
dfamrln <mhdffa1@gmail.com>
2025-06-05 17:58:58 +0700

    returnvalue

--- stack.cpp
+++ stack.cpp
@@ -72,6 +72,7 @@ public:
                 cout << current->data << " " << endl;
                 current = current->next;
             }
-        }
+            cout << endl;
+        }// return the value of the top node
     }
-}
+};
