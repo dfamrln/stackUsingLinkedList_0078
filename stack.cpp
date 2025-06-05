9e8b852fc497cad98f71487fe63865af4049e8a2
dfamrln <mhdffa1@gmail.com>
2025-06-05 15:35:32 +0700

    second commit

---
+++ stack.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Node class representing a single node in the linked list
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
+
+// stack class
