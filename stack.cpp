ba965546f6536424abb4e1332da20faf354767b6
dfamrln <mhdffa1@gmail.com>
2025-06-05 17:38:22 +0700

    allocate

--- stack.cpp
+++ stack.cpp
@@ -28,4 +28,9 @@ public:
 
      // push operation insert an element onto the top of the stack
     int push(int value)
+    {
+        Node *newNode = new Node(); // 1. allocate memory for the new node
+        newNode->data = value;      // 2. assign value
+        newNode->next = top;        // 3. set the next pointer of the new node to the current
+    }
 }
